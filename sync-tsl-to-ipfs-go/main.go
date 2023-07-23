8bcb5f8423f3baa284aaed96f83fbf39c912c976
Eric Stacey <ejstacey@joyrex.net>
2023-07-23 17:55:31 +1000

    A broken version of the go script

--- sync-tsl-to-ipfs-go/main.go
+++ sync-tsl-to-ipfs-go/main.go
@@ -11,7 +11,9 @@ import (
 
 	"github.com/adhocore/jsonc"
 	"github.com/davecgh/go-spew/spew"
-	shell "github.com/ipfs/go-ipfs-api"
+	pathMod "github.com/ipfs/boxo/path"
+	"github.com/ipfs/kubo/client/rpc"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 type ipfsFile struct {
@@ -53,7 +55,16 @@ type Settings struct {
 	Refresh              bool   `json:"Refresh"`
 	IpfsDbFileName       string `json:"IpfsDbFileName"`
 	TslDbFileName        string `json:"TslDbFileName"`
-	IpfsSession          *shell.Shell
+	IpfsSession          *rpc.HttpApi
+	MAddr                string
+}
+
+type dbOpts struct {
+	settings Settings
+	path     string
+	ipfsDb   ipfsDir
+	tslDb    tslDir
+	fullPath string
 }
 
 func grabCurrentIpfs(settings Settings, path string) ipfsDir {
@@ -64,9 +75,11 @@ func grabCurrentIpfs(settings Settings, path string) ipfsDir {
 	// spew.Dump(path)
 
 	var sh = settings.IpfsSession
+	var unixFs = sh.Unixfs()
 
 	ctx := context.Background()
-	ipfsFiles, err := sh.FilesLs(ctx, path, shell.FilesLs.Stat(true))
+	var pathObj = pathMod.Path(path)
+	ipfsFiles, err := unixFs.Ls(ctx, pathObj, shell.FilesLs.Stat(true))
 	if err != nil {
 		if !strings.Contains(err.Error(), "no such file or directory") {
 			fmt.Fprintf(os.Stderr, "error: %s", err)
@@ -108,43 +121,47 @@ func grabCurrentTsl(settings Settings, tslFsDir string) tslDir {
 		log.Fatal(err)
 	}
 
-	for _, file := range files {
-		dotDir := regexp.MustCompile(`^\.`)
-		if dotDir.MatchString(file.Name()) {
-			continue
-		}
+	if len(files) > 0 {
+		for _, file := range files {
+			dotDir := regexp.MustCompile(`^\.`)
+			if dotDir.MatchString(file.Name()) {
+				continue
+			}
 
-		var fsPath = tslFsDir + "/" + file.Name()
+			var fsPath = tslFsDir + "/" + file.Name()
+			lastDir := regexp.MustCompile(`^.+/([^/]+)$`)
+			tslDir.Name = lastDir.ReplaceAllString(tslFsDir, "$1")
+			tslDir.FsPath = tslFsDir
+
+			if file.Type().IsDir() {
+				// directory, go deeper
+				var contents = grabCurrentTsl(settings, fsPath)
+				tslDir.Dirs = append(tslDir.Dirs, contents)
+			} else if file.Type().IsRegular() {
+				// a file, what we expect.
+				var tslFile tslFile
+				tslFile.Name = file.Name()
+				var fileInfo, err = file.Info()
+				if err != nil {
+					log.Fatal(err)
+				}
+				tslFile.Size = fileInfo.Size()
+
+				var fullPath = fsPath
+				fullPath = strings.ReplaceAll(fullPath, settings.TslDirectory, settings.RemoteTslDirectory)
+				tslFile.Path = fullPath
+
+				var mfsPath = fsPath
+				mfsPath = strings.ReplaceAll(mfsPath, settings.TslDirectory, settings.IpfsMfsRootDirectory)
+				tslFile.MfsPath = mfsPath
+
+				tslDir.Files = append(tslDir.Files, tslFile)
+			}
+		}
+	} else {
 		lastDir := regexp.MustCompile(`^.+/([^/]+)$`)
 		tslDir.Name = lastDir.ReplaceAllString(tslFsDir, "$1")
 		tslDir.FsPath = tslFsDir
-
-		if file.Type().IsDir() {
-			// directory, go deeper
-			var contents = grabCurrentTsl(settings, fsPath)
-			tslDir.Dirs = append(tslDir.Dirs, contents)
-			tslDir.Name = file.Name()
-			tslDir.FsPath = fsPath
-		} else if file.Type().IsRegular() {
-			// a file, what we expect.
-			var tslFile tslFile
-			tslFile.Name = file.Name()
-			var fileInfo, err = file.Info()
-			if err != nil {
-				log.Fatal(err)
-			}
-			tslFile.Size = fileInfo.Size()
-
-			var fullPath = fsPath
-			fullPath = strings.ReplaceAll(fullPath, settings.TslDirectory, settings.RemoteTslDirectory)
-			tslFile.Path = fullPath
-
-			var mfsPath = fsPath
-			mfsPath = strings.ReplaceAll(mfsPath, settings.TslDirectory, settings.IpfsMfsRootDirectory)
-			tslFile.MfsPath = mfsPath
-
-			tslDir.Files = append(tslDir.Files, tslFile)
-		}
 	}
 	return tslDir
 }
@@ -172,6 +189,8 @@ func loadSettings() Settings {
 		}
 
 	}
+
+	settings.MAddr = "/ip4/" + settings.IpfsServer + "/tcp/" + settings.IpfsPort
 
 	return settings
 }
@@ -260,17 +279,166 @@ func loadTslDb(settings Settings) tslDir {
 	return tslDb
 }
 
+func compareDbs(opts dbOpts) {
+	if opts.fullPath != "" {
+		opts.fullPath += "/" + opts.path
+	} else {
+		opts.fullPath += opts.path
+	}
+	spew.Dump(opts.fullPath)
+	for _, ipfsFile := range opts.ipfsDb.Files {
+		var found = false
+		var update = false
+
+		// handle the files here
+		for _, tslFile := range opts.tslDb.Files {
+			// fmt.Println("Comparing files (ipfs source): " + ipfsFile.Name + " to " + tslFile.Name)
+			if tslFile.Name == ipfsFile.Name {
+				found = true
+				if tslFile.Size != int64(ipfsFile.Size) {
+					update = true
+					fmt.Println("updating: " + ipfsFile.Name)
+				}
+				break
+			}
+		}
+		if !found {
+			fmt.Println("removing: " + ipfsFile.Name)
+		} else {
+			if !update {
+				// fmt.Println("Match")
+			}
+		}
+	}
+
+	for _, tslFile := range opts.tslDb.Files {
+		var found = false
+
+		// handle the files here
+		for _, ipfsFile := range opts.ipfsDb.Files {
+			// fmt.Println("Comparing files (tsl source): " + ipfsFile.Name + " to " + tslFile.Name)
+			if ipfsFile.Name == tslFile.Name {
+				// fmt.Println("Match")
+				found = true
+				break
+			}
+		}
+		if !found {
+			fmt.Println("adding: " + tslFile.Name)
+		}
+	}
+
+	// handle the subdirectories here
+	for _, ipfsDir := range opts.ipfsDb.Dirs {
+		var found = false
+		var savedTslDir tslDir
+
+		for _, tslDir := range opts.tslDb.Dirs {
+			// fmt.Println("Comparing dirs (ipfs source): " + ipfsDir.Name + " to " + tslDir.Name)
+
+			if tslDir.Name == ipfsDir.Name {
+				found = true
+				// fmt.Println("Match")
+				savedTslDir = tslDir
+				break
+			}
+		}
+
+		if found {
+			var newOpts dbOpts
+			newOpts.settings = opts.settings
+			newOpts.path = ipfsDir.Name
+			newOpts.ipfsDb = ipfsDir
+			newOpts.tslDb = savedTslDir
+			newOpts.fullPath = opts.fullPath
+			compareDbs(newOpts)
+		} else {
+
+			fmt.Println("removing dir: " + ipfsDir.Name)
+		}
+	}
+	for _, tslDir := range opts.tslDb.Dirs {
+		var found = false
+
+		for _, ipfsDir := range opts.ipfsDb.Dirs {
+			// fmt.Println("Comparing dirs (tsl source): " + tslDir.Name + " to " + ipfsDir.Name)
+
+			if tslDir.Name == ipfsDir.Name {
+				// fmt.Println("Match")
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			fmt.Println("adding dir: " + tslDir.Name)
+		}
+	}
+}
+
+// def parsePaths(settings, path, ipfsDb, tslDb, fullPath=""):
+//     fullPath += path
+//     for path in ipfsDb:
+//         if path not in tslDb:
+//             # everything in this path needs to be removed from ipfs
+//             removeEntries(settings, ipfsDb[path])
+//             # print("remove: " + fullPath + path)
+//         else:
+//             # compare the entries.
+//             if path.endswith('/'):
+//                 # we have a deeper path, check it
+//                 parsePaths(settings, path, ipfsDb[path], tslDb[path], fullPath)
+//             else:
+//                 # we have a file, finally compare
+//                 # if settings['verify']:
+//                 #     hash = getIpfsHash(settings, ipfsDb[path]['Path'])
+//                 #     if ipfsDb[path]['Hash'] != hash:
+//                 #         print("No match, reupload.")
+//                 # else:
+//                 # pp.pprint(ipfsDb[path])
+//                 # pp.pprint(tslDb[path])
+//                 # pp.pprint(path)
+//                 if ipfsDb[path]['Size'] != tslDb[path]['size']:
+//                     print("update: " +  fullPath + path)
+
+//     for path in tslDb:
+//         if path not in ipfsDb:
+//             # everything in this path needs to be added to ipfs
+//             # print("add: " + fullPath + path)
+//             if path.endswith('/'):
+//                 # we have a deeper path, check it
+//                 parsePaths(settings, path, {}, tslDb[path], fullPath)
+//             else:
+//                 addEntries(settings, tslDb[path])
+
 func main() {
 	fmt.Println("Loading settings.")
 	var settings = loadSettings()
 	fmt.Println("Settings Loaded.")
 
-	sh := shell.NewShell(settings.IpfsServer + ":" + settings.IpfsPort)
+	m1, err := ma.NewMultiaddr("/ip4/127.0.0.1/udp/1234")
+	if err != nil {
+		log.Fatal("Trying to create multiaddr entry with " + settings.IpfsServer + ":" + settings.IpfsPort + ": " + err.Error())
+	}
+	sh, err := rpc.NewApi(m1)
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+
+	// sh := shell.NewShell(settings.IpfsServer + ":" + settings.IpfsPort)
 	settings.IpfsSession = sh
 
 	var ipfsDb = loadIpfsDb(settings)
 	var tslDb = loadTslDb(settings)
 
-	spew.Dump(ipfsDb)
-	spew.Dump(tslDb)
+	var opts dbOpts
+	opts.settings = settings
+	opts.path = settings.IpfsMfsRootDirectory
+	opts.ipfsDb = ipfsDb
+	opts.tslDb = tslDb
+	opts.fullPath = ""
+	compareDbs(opts)
+	// spew.Dump(ipfsDb)
+	// spew.Dump(tslDb)
 }
